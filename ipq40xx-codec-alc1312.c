d1a29b6213234d844ae2e699006f7b1b8f89024f
Jeff Williams <jeff@wdwconsulting.net>
2023-09-26 16:31:55 -0700

    updating to latest kernel

--- ipq40xx-codec-alc1312.c
+++ ipq40xx-codec-alc1312.c
@@ -1261,11 +1261,11 @@ static const struct regmap_config alc1312_regmap_config = {
 	.reg_bits = 16,
 	.val_bits = 16,
 
-	.readable_reg = alc1312_readable_register,
-	.volatile_reg = alc1312_volatile_register,
+//	.readable_reg = alc1312_readable_register,
+//	.volatile_reg = alc1312_volatile_register,
 	.max_register = 0x8FF,
-	.reg_defaults = alc1312_reg,
-	.num_reg_defaults = ARRAY_SIZE(alc1312_reg),
+//	.reg_defaults = alc1312_reg,
+//	.num_reg_defaults = ARRAY_SIZE(alc1312_reg),
 //	.cache_type = REGCACHE_RBTREE,
 };
 
