a7a088eb98ac2327dc5d44b3880d35aef3f1dc8f
Jeff Williams <jeff@wdwconsulting.net>
2023-10-02 22:12:44 -0700

    update to latest kernel

--- ipq40xx.c
+++ ipq40xx.c
@@ -108,7 +108,7 @@ static int ipq40xx_soc_probe(struct ipq40xx_soc_priv *priv){
 //	priv->dai_links[0].cpus->dai_name = "qca-cpu-dai";
 	priv->dai_links[0].cpus->dai_name = "qca-i2s-dai";
         priv->dai_links[0].cpus->of_node = dai_node;
-	priv->dai_links[0].platforms->dai_name = "qca-pcm-i2s",
+//	priv->dai_links[0].platforms->dai_name = "qca-pcm-i2s",
 	priv->dai_links[0].platforms->name = "qca-pcm-i2s",
         priv->dai_links[0].platforms->of_node = platform_node;
 //        priv->dai_links[0].codecs->of_node = codec_node;
