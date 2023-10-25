2df5e97bfc9769ac5979602bcf4fa88656b98c88
Jeff Williams <jeff@wdwconsulting.net>
2023-10-25 18:22:01 +0000

    refactoring

--- ipq40xx-pcm-i2s.c
+++ ipq40xx-pcm-i2s.c
@@ -446,16 +446,20 @@ static int ipq40xx_pcm_i2s_hw_params(struct snd_soc_component *component,
 				substream->runtime->private_data;
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *dai = asoc_rtd_to_cpu(rtd, 0);
+	uint32_t bit_width, channels, rate;
 	uint32_t intf = dai->driver->id;
 	uint32_t mbox_id = get_mbox_id(substream, intf);
 	uint32_t stereo_id = get_stereo_id(substream, intf);
 	int ret;
 	unsigned int period_size, sample_size, sample_rate, frames, channels;
-
 	pr_debug("%s %d\n", __func__, __LINE__);
 
 	pcm_rtpriv = runtime->private_data;
 	printk("Keen %s %d\r\n",__func__,__LINE__);
+	bit_width = params_format(hw_params);
+	channels = params_channels(hw_params);
+	rate = params_rate(hw_params);
+
 	ipq40xx_config_master(ENABLE, stereo_id);
 
 	printk("Keen %s %d\r\n",__func__,__LINE__);
@@ -528,13 +532,14 @@ static int ipq40xx_pcm_i2s_open(struct snd_soc_component *component,
 	struct ipq40xx_pcm_rt_priv *pcm_rtpriv;
     struct snd_soc_pcm_runtime *rtd = substream->private_data;
     struct snd_soc_dai *dai = asoc_rtd_to_cpu(rtd, 0);
-	uint32_t intf = dai->driver->id;
+    uint32_t intf = dai->driver->id;
 	printk("%s %d\n", __func__, __LINE__);
 
 	pcm_rtpriv = kmalloc(sizeof(struct ipq40xx_pcm_rt_priv), GFP_KERNEL);
 	if (!pcm_rtpriv) {
 		return -ENOMEM;
 	}
+
 	snd_printd("%s: 0x%xB allocated at 0x%08x\n",
 			__FUNCTION__, sizeof(*pcm_rtpriv), (u32) pcm_rtpriv);
 	pcm_rtpriv->last_played = NULL;
